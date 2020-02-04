a237c26323f749f788c53880993ad7a7210371af
Jose Fretel <josefretel26@hotmail.com>
2020-02-04 11:13:02 -0500

    Angular + SQL SERVER - BASIC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreSqlServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/FavoriteBand.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace JudgeMyTaste.Models
+{
+  public class FavoriteBand
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string EnteredBy { get; set; }
+    public DateTime EnteredOn { get; set; }
+  }
+}
